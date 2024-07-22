ba43d82b857dcf4d7bc852139bce3424ab2ea7fd
Alaaddin Gursoy <alaaddin64.ag@gmail.com>
2024-07-22 23:56:27 +0300

    Duzeltme

--- Unity OUA Project/Assets/Scripts/Player.cs
+++ Unity OUA Project/Assets/Scripts/Player.cs
@@ -8,8 +8,6 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Joystick joystick;
 
-    [SerializeField] private Transform astranoutTransform;
-
     [SerializeField] private float forwardSpeed;
     [SerializeField] private float leftRightSpeed;
 
@@ -20,11 +18,10 @@ public class Player : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
+    // Update is called once per frame
     private void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinMaxX.x, MinMaxX.y), transform.position.y, transform.position.z);
         _rb.velocity = new Vector3(joystick.Horizontal* leftRightSpeed * Time.deltaTime, _rb.velocity.y, forwardSpeed* Time.deltaTime);
-
-        astranoutTransform.rotation = Quaternion.Euler(0,joystick.Horizontal*45, 0);
     }
 }
