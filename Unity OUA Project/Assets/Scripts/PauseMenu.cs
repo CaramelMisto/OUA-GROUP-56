d46681cc97ed8689527ed749c14b068a08c8899e
Alaaddin Gursoy <alaaddin64.ag@gmail.com>
2024-07-23 22:33:06 +0300

    Pause menusu eklendi. Not: Kullanilmayacak UI assetlerinin ileride kaldirilmasi iyi bir pratik olacaktir.

---
+++ Unity OUA Project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    public void Pause() { 
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Home() {
+        SceneManager.LoadScene("EntryScene");
+        Time.timeScale = 1f;
+    }
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene("Gameplay");
+        Time.timeScale = 1f;
+    }
+}
